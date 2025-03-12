d0221297cd768e6e5652059d7490c4b8b8650939
osman-jalloh-lab <osman.jalloh@g.austincc.edu>
2025-03-12 04:24:05 +0000

    question 3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,12 @@
 #add import
+# Define the function
+def use_local_variable(num):
+    num = 10  # Local variable, only exists inside the function
+
+# Main program
+if __name__ == "__main__":
+    num = 50  # External variable
+    print(f"Before calling the function, num = {num}")  # Display the initial value
+    use_local_variable(num)  # Call the function
+    print(f"After calling the function, num = {num}")  # Display the value after the function call
+
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,9 @@
 #write functions here, don't add input('') statements here!
+def use_local_variable(num):
+    num = 10  # This is a local variable, it won't affect variables outside this function
+
+
+num = 100
+use_local_variable (num)
+
+print("Value of num in the test case:", num)
